f7acf8b64f3643f86c979213dba414e59ac26187
simon-frank <38725429+simon-frank@users.noreply.github.com>
2018-06-25 11:14:10 +0100

    Update Anforderungsanalyse.java

--- src/model/Anforderungsanalyse.java
+++ src/model/Anforderungsanalyse.java
@@ -2,14 +2,17 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import nachkalkulation.*;
 // Singleton
 public class Anforderungsanalyse {
     
     // Kein Interface arghh. Dein Ernst
     private List<Produktfunktion> produktfunktionen;
     private List<Produktdaten> produktdaten;
-
+    private Faktoren userfaktoren;
+    private Faktoren sollfaktoren;
+    private Optimieren_I optimieren;
+    private FunctionPoints fp;
     private static Anforderungsanalyse anforderungsanalyse;
 
     private Anforderungsanalyse() {
@@ -39,6 +42,11 @@ public class Anforderungsanalyse {
 
     public void setProduktdaten(List<Produktdaten> produktdaten) {
         this.produktdaten = produktdaten;
+    }
+   
+    public void selbstoptimierung(){
+        this.sollfaktoren=this.optimieren.optimieren(this.fp.getIstfp(), this.fp.getSollfp, this.userfaktoren.getFaktoren());
+    
     }
 
 }
