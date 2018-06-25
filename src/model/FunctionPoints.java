c6feb6163dee7fb9a65de73aa6b2cf68d88b8e23
simon-frank <38725429+simon-frank@users.noreply.github.com>
2018-06-25 11:49:33 +0100

    Add files via upload

--- src/model/FunctionPoints.java
+++ src/model/FunctionPoints.java
@@ -1,5 +1,38 @@
 package nachkalkulation;
+import aufwandsabschaetzung.*;
 
 public class FunctionPoints {
+	private double istfp;
+	private double sollfp;
+	private double mannmonate;
+	private Konfiguration_I config;
+	
+	public FunctionPoints(){
+		this.istfp=0;
+		this.sollfp=0;
+		this.config= new Konfiguration();
+	}
 
+	public double getIstfp() {
+		return istfp;
+	}
+
+	public void setIstfp(double istfp) {
+		this.istfp = istfp;
+	}
+
+	public double getSollfp() {
+		return sollfp;
+	}
+
+
+	public double getMannmonate() {
+		return mannmonate;
+	}
+
+	public void setMannmonate(double mannmonate) {
+		this.mannmonate = mannmonate;
+		this.sollfp= this.config.calcfp(mannmonate);
+		
+	}
 }
