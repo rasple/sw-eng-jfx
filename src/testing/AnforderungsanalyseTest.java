dd649f9d973fd3889aed431066c6d69be87e145f
K6205 <39126951+K6205@users.noreply.github.com>
2018-07-02 20:01:59 +0100

    please work

--- src/testing/AnforderungsanalyseTest.java
+++ src/testing/AnforderungsanalyseTest.java
@@ -7,6 +7,6 @@ public class AnforderungsanalyseTest {
     @Test
     public void evaluatesExpression(){
 
-
+        //also, this will hae to get done tomorrow, been spending 3 hours getting any push rejected
     }
 }
