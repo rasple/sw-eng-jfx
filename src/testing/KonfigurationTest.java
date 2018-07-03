fa1abc987fda2981a436cde5261ec876df68d5b3
K6205 <39126951+K6205@users.noreply.github.com>
2018-07-03 19:31:42 +0100

    Simon du dummer HS wenn ich bei der calcmannmonate für fp 2600 einsetze kommt -101 raus bitte mal beheben

--- src/testing/KonfigurationTest.java
+++ src/testing/KonfigurationTest.java
@@ -34,4 +34,23 @@ public class KonfigurationTest {
         assertEquals(HashMap.class, HMDaten.getClass());
     }
 
+    @Test
+    public void evaluateCalcMannMonate(){
+
+        Konfiguration config = new Konfiguration();
+        assertEquals(0, config.calcmannmonate(0), 0);
+        assertEquals(0, config.calcmannmonate(50), 0);
+        assertEquals(76, config.calcmannmonate(1000), 0);
+        assertEquals(112, config.calcmannmonate(1400), 0);
+        assertEquals(142, config.calcmannmonate(1700), 0);
+        assertEquals(175, config.calcmannmonate(2000), 0);
+        assertEquals(201, config.calcmannmonate(2200), 0);
+        assertEquals(245, config.calcmannmonate(2500), 0);
+        assertEquals(267, config.calcmannmonate(2600), 0);
+        assertEquals(284, config.calcmannmonate(2700), 0);
+        assertEquals(287, config.calcmannmonate(2800), 0);
+        assertEquals(341, config.calcmannmonate(2900), 0);
+        assertEquals(341, config.calcmannmonate(3000), 0);
+    }
+
 }
