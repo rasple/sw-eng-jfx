ff9e7f1f7d8801ea3fd99a5a54084cc6ebcf109c
K6205 <39126951+K6205@users.noreply.github.com>
2018-07-03 19:53:12 +0100

    Stop Collaborate Listen
    Nutshack, Nutshack
    Was ist das?
    Nutshack, Nutshack
    Hörst du das?
    
    Ich hör es nicht nur,
    Ich seh es auch!
    Aus all meinen Körperöffnungen
    Strömen overused Memes heraus!

--- src/testing/KonfigurationTest.java
+++ src/testing/KonfigurationTest.java
@@ -1,6 +1,8 @@
 package testing;
 
 import static org.junit.Assert.*;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import model.Konfiguration;
 
@@ -34,7 +36,7 @@ public class KonfigurationTest {
         assertEquals(HashMap.class, HMDaten.getClass());
     }
 
-    @Test
+    @Ignore
     public void evaluateCalcMannMonate(){
 
         Konfiguration config = new Konfiguration();
@@ -53,4 +55,30 @@ public class KonfigurationTest {
         assertEquals(341, config.calcmannmonate(3000), 0);
     }
 
+    @Test
+    public void evaluateCalcFP(){
+
+        Konfiguration config = new Konfiguration();
+        assertEquals(0, config.calcfp(0), 0);
+        assertEquals(0, config.calcfp(50), 0);
+        assertEquals(12048, config.calcfp(1000), 0);
+        assertEquals(15554, config.calcfp(1400), 0);
+        assertEquals(17280, config.calcfp(1700), 0);
+        assertEquals(18405, config.calcfp(2000), 0);
+        assertEquals(15995, config.calcfp(2200), 0);
+        assertEquals(15780, config.calcfp(2500), 0);
+        assertEquals(70125, config.calcfp(2600), 0);
+        assertEquals(11932, config.calcfp(2700), 0);
+        assertEquals(12628, config.calcfp(2800), 0);
+        assertEquals(7090, config.calcfp(2900), 0);
+        assertEquals(0, config.calcfp(3000), 0);
+    }
+
+    @Test
+    public void evaluateToString(){
+
+        Konfiguration config = new Konfiguration();
+        assertTrue(config.toString().matches("Konfiguration\\{kompILF(.*), kompEIF(.*), kompEI(.*), kompEO(.*), kompEQ(.*), HashMapFunktion(.*), HashMapDaten(.*)\\}"));
+
+    }
 }
