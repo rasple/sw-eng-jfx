26417a876a033f53d8ee49c2d970f6bfbb7ce399
Schwartzhammer Klaus <39126951+K6205@users.noreply.github.com>
2018-07-04 13:46:56 +0100

    updated ProduktdatenTest.java

--- src/testing/ProduktdatenTest.java
+++ src/testing/ProduktdatenTest.java
@@ -2,12 +2,53 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import model.Konfiguration;
+import model.Produktdaten;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import model.Produktdaten_I;
+import model.Konfiguration_I;
+
+import java.util.HashMap;
+
 public class ProduktdatenTest {
 
+    @Test
+    public void calcFpTest(){
 
+        Konfiguration config = new Konfiguration();
+        Produktdaten produktdaten = new Produktdaten("Test", "test", 1, 1, "EI");
+        assertEquals(3, produktdaten.calcFp(config.getHashMapFunktion()));
 
+        produktdaten.setType("EO");
+        assertEquals(4, produktdaten.calcFp(config.getHashMapFunktion()));
+
+        produktdaten.setType("EQ");
+        assertEquals(3, produktdaten.calcFp(config.getHashMapFunktion()));
+
+    }
+
+    @Test
+    public void isValidTest(){
+
+        Produktdaten produktdaten = new Produktdaten("Test", "test", 1, 1, "EI");
+        assertTrue(produktdaten.isValid());
+
+        produktdaten = new Produktdaten();
+        assertFalse(produktdaten.isValid());
+
+        produktdaten = new Produktdaten();
+        produktdaten.setRet(1);
+        produktdaten.setDet(1);
+        assertFalse(produktdaten.isValid());
+
+        produktdaten = new Produktdaten();
+        produktdaten.setType("EO");
+        produktdaten.setRet(-1);
+        produktdaten.setDet(0);
+        assertFalse(produktdaten.isValid());
+
+    }
 
 }
