61bb30236f347418ef1c3e9f1b171984827de6aa
Schwartzhammer Klaus <39126951+K6205@users.noreply.github.com>
2018-07-04 10:45:22 +0100

    trhrw

--- src/model/Anforderungsanalyse.java
+++ src/model/Anforderungsanalyse.java
@@ -101,7 +101,7 @@ public class Anforderungsanalyse implements Serializable, Cloneable, Anforderung
         }
         while (iteratorFkt.hasNext()) {
             currentFkt = iteratorFkt.next();
-            if (currentFkt.isvalid()) {
+            if (currentFkt.isValid()) {
                 unbewerteFP += currentFkt.calcFp(config.getHashMapFunktion());
             }
             else{
@@ -113,7 +113,7 @@ public class Anforderungsanalyse implements Serializable, Cloneable, Anforderung
         while(iteratorda.hasNext()){
             currentDa = iteratorda.next();
             if (currentDa.isValid()) {
-                unbewerteFP += currentDa.calcFP(config.getHashMapDaten());
+                unbewerteFP += currentDa.calcFp(config.getHashMapDaten());
             }
             else{
                 return -2;
