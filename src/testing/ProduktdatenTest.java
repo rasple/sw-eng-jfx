04936f54966bea9a9e99765b999745acbb22ddc7
simon-frank <simon.frank98@web.de>
2018-07-04 17:56:50 +0100

    Alle Fälle bei den Produktdaten testen

--- src/testing/ProduktdatenTest.java
+++ src/testing/ProduktdatenTest.java
@@ -20,9 +20,55 @@ public class ProduktdatenTest {
         Konfiguration config = new Konfiguration();
         Produktdaten produktdaten = new Produktdaten("Test", "test", 1, 1, "ILF");
         assertEquals(7, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setDet(20);
+        assertEquals(7, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setDet(51);
+        assertEquals(10, produktdaten.calcFp(config.getHashMapDaten()));
+
+        produktdaten.setRet(2);
+        produktdaten.setDet(19);
+        assertEquals(7, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setRet(3);
+        produktdaten.setDet(50);
+        assertEquals(10, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setDet(57);
+        assertEquals(15, produktdaten.calcFp(config.getHashMapDaten()));
+
+        produktdaten.setRet(6);
+        produktdaten.setDet(15);
+        assertEquals(10, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setDet(25);
+        assertEquals(15, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setDet(70);
+        assertEquals(15, produktdaten.calcFp(config.getHashMapDaten()));
 
         produktdaten.setType("EIF");
+        produktdaten.setRet(1);
+        produktdaten.setDet(1);
         assertEquals(5, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setDet(20);
+        assertEquals(5, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setDet(51);
+        assertEquals(7, produktdaten.calcFp(config.getHashMapDaten()));
+
+        produktdaten.setRet(2);
+        produktdaten.setDet(19);
+        assertEquals(5, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setRet(3);
+        produktdaten.setDet(50);
+        assertEquals(7, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setDet(57);
+        assertEquals(10, produktdaten.calcFp(config.getHashMapDaten()));
+
+        produktdaten.setRet(6);
+        produktdaten.setDet(15);
+        assertEquals(7, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setDet(25);
+        assertEquals(10, produktdaten.calcFp(config.getHashMapDaten()));
+        produktdaten.setRet(8);
+        produktdaten.setDet(70);
+        assertEquals(10, produktdaten.calcFp(config.getHashMapDaten()));
+
 
     }
 
