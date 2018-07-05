4527df8fb458ea4f0a4bdc06e30163b000bd7a89
Schwartzhammer Klaus <39126951+K6205@users.noreply.github.com>
2018-07-05 15:00:20 +0100

    fixed OptimierenTest

--- src/testing/OptimierenTest.java
+++ src/testing/OptimierenTest.java
@@ -14,13 +14,13 @@ public class OptimierenTest {
     public void kleinerOptimieren(){
 
         double[] testIstFp = new double[]{1, 5, 10, 50, 100};
-        double[] testSollFp = new double[]{0, 3, 7.4, 2, 85};
+        double[] testSollFp = new double[]{0, 3, 74, 2, 85};
         double[][] testFactors = new double[][]{
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
-                {0.498, 2.421, 3.67898, 3.44, 9.775, 1.24, 0.0154, 5, 4, 1},
+                {0.498, 2.421, 3.67898, 3.44, 4.775, 1.24, 0.0154, 5, 4, 1},
                 {1, 2, 3, 4, 5, 4, 3, 2, 1, 0},
-                {4, 2, 7, 3, 9, 1, 0, 5, 0, 7}
+                {4, 2, 7, 3, 2, 1, 0, 5, 0, 5}
         };
 
         Optimieren_I opti = new DefaultOptimierung();
@@ -34,8 +34,9 @@ public class OptimierenTest {
             for(Object factor : fact0rs){
 
                 assertThat(factor, instanceOf(double.class));
-                assert((double) factor >= 0);
-                assert((double) factor <= 5);
+                double dfactor = (double) factor;
+                assertTrue(dfactor >= 0);
+                assertTrue(dfactor <= 5);
             }
         }
     }
